1b1c0f94d726f6542dbd59b3942b8cf5eca2d694
cococode <113681517+chloehage@users.noreply.github.com>
2023-05-25 23:27:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pygame
 from random import randint
+import imputbox
 
 pygame.init()
 
@@ -10,6 +11,7 @@ pygame.display.set_caption("THERE IS NO GAME")
 x,y = 370,370
 image = pygame.image.load("du-son.png")
 image = pygame.transform.scale(image, (100,100))
+image.set_alpha(255)
 musique = pygame.mixer.music.load("ascenseur.mp3")
 ecran.blit(image, (x,y))
 
@@ -18,11 +20,9 @@ def coupe_music(event,x,y,mouse):
     if event.type == pygame.MOUSEBUTTONDOWN:
         if x<mouse[0] and mouse[0]<x+100 and y<mouse[1] and mouse[1]<y+100:
             pygame.mixer.music.pause()
+            image.set_alpha(0)
     pygame.display.flip()
     
-def nom(event,mouse):
-    if event.type == pygame.K_SPACE:
-        pygame.mixer.music.play()
 
 def multiple(event,x,y,mouse):
     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -36,16 +36,12 @@ def multiple(event,x,y,mouse):
 running = True
 pygame.mixer.music.play()
 
+
 while running == True:
     mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        coupe_music(event,x,y,mouse)
-        nom(event,mouse)
+    multiple(event,x,y,mouse)
         
-
-    
-    
-    
 pygame.quit()
