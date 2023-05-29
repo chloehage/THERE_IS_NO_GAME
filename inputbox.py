88a10212598807650bfd677c893410a538eec907
cococode <113681517+chloehage@users.noreply.github.com>
2023-05-29 22:07:56 +0200

    Add files via upload

--- inputbox.py
+++ inputbox.py
@@ -2,19 +2,23 @@ import pygame as pygame
 
 class InputBox():
     def __init__(self, x, y, w, h, text="ENTRE TON NOM"):
+        # création d'un rectangle
         self.rect = pygame.Rect(x, y, w, h)
+        # couleur de base
         self.color_inactive = pygame.Color("white")
+        # couleur quand on active le bouton
         self.color_active = pygame.Color("white")
+        # couleur par défaut
         self.color = self.color_inactive
         self.text = text
-        self.font = pygame.font.Sysfont("Arial", 18, 0)
+        self.font = pygame.font.SysFont("Arial", 18, 0)
         self.txt_surface = self.font.render(text, True, self.color)
         self.active = False
         self.fait = False
     
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
+            if self.rect.collidepoint(event.pos): # si la souris est sur le bouton
                 self.active = not self.active
                 self.text = ""
             else:
@@ -25,20 +29,20 @@ class InputBox():
                 self.color = self.color_inactive
             if event.type == pygame.KEYDOWN:
                 if self.active:
-                    if event.key == pygame.K_RETURN:
+                    if event.key == pygame.K_RETURN: # entrer le nom
                         print(self.text)
                         self.fait = True
-                    elif event.key == pygame.K_BACKSPACE:
+                    elif event.key == pygame.K_BACKSPACE: # supprimer le dernier caractère
                         self.text = self.text[:-1]
                     else:
-                        self.text += event.unicode
+                        self.text += event.unicode # ajouter un caractère
                     self.txt_surface = self.font.render(self.text, True, self.color)
                     
     def update(self):
         width = max(180, self.txt_surface.get_width()+10)
         self.rect.w = width
         
-    def draw(self, screen):
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
-        pygame.draw.rect(screen, self.color, self.rect, 2)
+    def draw(self, ecran):
+        ecran.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.rect(ecran, self.color, self.rect, 2)
                         
--- main.py
+++ main.py
@@ -3,56 +3,79 @@ from random import randint
 import inputbox
 
 def coupe_music():
+    """
+Fonction pour couper la musique quand on clique sur le bouton de volume
+"""
     pygame.mixer.music.pause()
     
 
 def multiple(x,y,image):
+    """
+Fonction pour afficher un nouveau bouton de volume quand on clique sur le premier bouton
+"""
     for i in range(10):
         x,y = randint(0,700), randint(0,700)
         print(x,y)
         ecran.blit(image, (x,y))
         pygame.display.flip()
         
-def nom(inputbox,event):
-    inputbox.handle_event(event)
+def nom(inputbox,ecran):
+    """
+Fonction pour entrer le nom de l'utilisateur
+"""
+    for event in pygame.event.get():
+        inputbox.handle_event(event)
+    inputbox.update()
+    inputbox.draw(ecran)
+    pygame.display.flip()
     
-
+# lancement des modules inclus dans pygame
 pygame.init()
-
+# création d'une fenêtre de 800 par 800
 ecran = pygame.display.set_mode((800,800))
 pygame.display.set_caption("THERE IS NO GAME")
 
 
 x,y = 370,370
+
 image = pygame.image.load("du-son.png")
 image = pygame.transform.scale(image, (100,100))
 image.set_alpha(255)
 musique = pygame.mixer.music.load("ascenseur.mp3")
-ecran.blit(image, (x,y))
 
 running = True
 pygame.mixer.music.play()
+# variable pour enchaîner les différentes fonctions
 state = 0
-inputbox = inputbox.InputBox(x,y,400,36)
+# chargement du bouton pour entrer le nom d'utilisateur
+inputbox = inputbox.InputBox(310,370,140,32)
+ecran.blit(image, (x,y))
 
+# boucle infinie pour laisser la fenêtre ouverte
 while running == True:
     mouse = pygame.mouse.get_pos()
-    ecran.blit(image, (x,y))
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # si on clique sur le bouton volume
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if x<mouse[0] and mouse[0]<x+100 and y<mouse[1] and mouse[1]<y+100:
                 if state == 0:
                     coupe_music()
                     state += 1
                 elif state == 1:
-                    nom(inputbox,event)
+                    pygame.Surface.fill(ecran, [0,0,0])
+                    nom(inputbox,ecran)
                     state += 1
                 elif state == 2:
+                    pygame.Surface.fill(ecran, [0,0,0])
+                    ecran.blit(image, (x,y))
+                    state += 1
+                elif state == 3:
                     multiple(x,y,image)
                     pygame.mixer.music.play()
                     
+              
         
 pygame.quit()
