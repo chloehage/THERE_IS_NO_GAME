aaa3e02f9dd07bf842f5f4ddbba9d33d8415f2e7
cococode <113681517+chloehage@users.noreply.github.com>
2023-05-29 19:44:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import pygame as pygame
 from random import randint
 import inputbox
 
+def coupe_music():
+    pygame.mixer.music.pause()
+    
+
+def multiple(x,y,image):
+    for i in range(10):
+        x,y = randint(0,700), randint(0,700)
+        print(x,y)
+        ecran.blit(image, (x,y))
+        pygame.display.flip()
+        
+def nom(inputbox,event):
+    inputbox.handle_event(event)
+    
+
 pygame.init()
 
 ecran = pygame.display.set_mode((800,800))
@@ -15,33 +30,29 @@ image.set_alpha(255)
 musique = pygame.mixer.music.load("ascenseur.mp3")
 ecran.blit(image, (x,y))
 
-def coupe_music(event,x,y,mouse):
-    ecran.blit(image, (x,y))
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if x<mouse[0] and mouse[0]<x+100 and y<mouse[1] and mouse[1]<y+100:
-            pygame.mixer.music.pause()
-            image.set_alpha(0)
-    pygame.display.flip()
-    
-
-def multiple(event,x,y,mouse):
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if x<mouse[0] and mouse[0]<x+100 and y<mouse[1] and mouse[1]<y+100:
-            x,y = randint(0,700), randint(0,700)
-            print(x,y)
-            ecran.blit(image, (x,y))
-    pygame.display.flip()
-    
-
 running = True
 pygame.mixer.music.play()
-
+state = 0
+inputbox = inputbox.InputBox(x,y,400,36)
 
 while running == True:
     mouse = pygame.mouse.get_pos()
+    ecran.blit(image, (x,y))
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    multiple(event,x,y,mouse)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if x<mouse[0] and mouse[0]<x+100 and y<mouse[1] and mouse[1]<y+100:
+                if state == 0:
+                    coupe_music()
+                    state += 1
+                elif state == 1:
+                    nom(inputbox,event)
+                    state += 1
+                elif state == 2:
+                    multiple(x,y,image)
+                    pygame.mixer.music.play()
+                    
         
 pygame.quit()
